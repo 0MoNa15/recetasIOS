6a9ed2e608541a612d791599fad6de66b39f6372
Zorayda <zorayda.gutierrez@ceiba.com.co>
2023-12-21 07:54:59 -0500

    Creación de módulo de infra y dominio

---
+++ recetasIOS/infraestructure/infraestructure.h
@@ -0,0 +1,18 @@
+//
+//  infraestructure.h
+//  infraestructure
+//
+//  Created by Zorayda Yadira Gutierrez Montes - Ceiba Software on 21/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for infraestructure.
+FOUNDATION_EXPORT double infraestructureVersionNumber;
+
+//! Project version string for infraestructure.
+FOUNDATION_EXPORT const unsigned char infraestructureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <infraestructure/PublicHeader.h>
+
+
---
+++ recetasIOS/recetasIOS/recetasIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  recetasIOSApp.swift
+//  recetasIOS
+//
+//  Created by Zorayda Yadira Gutierrez Montes - Ceiba Software on 21/12/23.
+//
+
+import SwiftUI
+
+@main
+struct recetasIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
